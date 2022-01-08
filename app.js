f881ff4860ec81227f6ee684ce6fe6eabaae70ac
klapnitskiy <kosov199820@gmail.com>
2022-01-08 02:11:40 +0200

    added transition

--- app.js
+++ app.js
@@ -1,47 +1,63 @@
-const bgContainer = document.querySelector('.background')
+window.addEventListener('load',() => {
+    const bgContainer = document.querySelector('.background');
+    const bgClasses = ['background background--bg1', 'background background--bg2', 'background background--bg3'];
+    const COOKIE_NAME = 'bgName';
+    const COOKIE_INTERVAL = 10000;
+    const COOKIE_EXP = 10000;
 
-bgClasses = ['background background--bg1', 'background background--bg2', 'background background--bg3']
-
-// Set a Cookie
-function setCookie(cName, cValue, expSecs) {
-    let date = new Date();
-    date.setTime(date.getTime() + expSecs);
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
-}
-
-function getCookie(name) {
-    let matches = document.cookie.match(new RegExp(
-        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-}
-
-function initialImg() {
-    if (document.cookie) {
-        bgContainer.className = getCookie('bgName')
-    } else {
-        console.log('document.cookie')
-        const getPic = bgClasses[Math.floor(Math.random() * bgClasses.length)];
-        setCookie('bgName', getPic, 10000);
-        bgContainer.className = getPic
-        console.log(document.cookie);
+    function setCookie(cName, cValue, expSecs) {
+        let date = new Date();
+        date.setTime(date.getTime() + expSecs);
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
     }
-}
 
-initialImg()
+    function getCookie(name) {
 
-setInterval(() => {
-    const cookieValue = getCookie('bgName')
+        let cookies = undefined;
 
-    if (!cookieValue) {
-        console.log('document.cookie')
-        const getPic = bgClasses[Math.floor(Math.random() * bgClasses.length)];
-        setCookie('bgName', getPic, 10000);
-        bgContainer.className = getPic
-        console.log(document.cookie);
+        if (document.cookie) {
+            document.cookie.split(';').forEach(item => {
+                const [cookieName, cookieValue] = item.split('=')
+                if (cookieName === name) {
+                    cookies = cookieValue;
+                }
+            })
+        }
+
+        return cookies;
     }
-}, 2000)
+
+    function initialImg() {
+        const bgClass = getCookie('bgName');
 
 
+        if (bgClass) {
+            setClassAnim(bgClass)
+        } else {
+            setImg();
+        }
+    }
 
+    function setClassAnim (bgClassName) {
+        bgContainer.classList.add('fadeOut');
+        bgContainer.addEventListener('transitionend', () => {
+            bgContainer.className = bgClassName;
+        }, {once: true});
+    }
+
+    function setImg() {
+        const getPic = bgClasses[Math.floor(Math.random() * bgClasses.length)];
+        setCookie(COOKIE_NAME, getPic, COOKIE_EXP);
+        setClassAnim(getPic);
+    }
+
+    initialImg();
+
+    setInterval(() => {
+        const cookieValue = getCookie(COOKIE_NAME);
+        if (!cookieValue) {
+            setImg();
+        }
+    }, COOKIE_INTERVAL);
+})
